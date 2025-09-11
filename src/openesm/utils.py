af4343feaa1a7e82675510828c022dbe2833e5e1
Bjoern Siepe <bjoernsiepe@gmail.com>
2025-09-11 09:37:00 +0200

    add `n_days` to study metadata

--- src/openesm/utils.py
+++ src/openesm/utils.py
@@ -289,6 +289,7 @@ def process_specific_metadata(raw_meta: dict[str, Any]) -> dict[str, Any]:
         "link_to_code": get_val("link_to_code"),
         "n_participants": get_val("n_participants", "int"),
         "n_time_points": get_val("n_time_points", "int"),
+        "n_days": get_val("n_days", "int"),
         "n_beeps_per_day": get_val("n_beeps_per_day"),
         "passive_data_available": get_val("passive_data_available"),
         "cross_sectional_available": get_val("cross_sectional_available"),
